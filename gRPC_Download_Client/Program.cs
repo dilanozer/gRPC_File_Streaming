b59f5f75d3efc19d10d9294ad61f7b27f63c2413
Dilan ÖZER <dilanozer@Dilans-MacBook-Air.local>
2024-08-09 13:47:07 +0300

    grpc kutuphanesi ile file upload be file download uygulamasi

---
+++ gRPC_Download_Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using Grpc.Net.Client;
+using gRPC_Download_Client.Protos;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5263");
+var client = new FileService.FileServiceClient(channel);
+
+string downloadPath = @"/Users/dilanozer/Projects/gRPCFileStreaming/gRPC_Download_Client/DownloadFiles";
+
+var fileInfo = new gRPC_Download_Client.Protos.FileInfo
+{
+    FileName = "beceri",
+    FileExtension = ".mp4"
+};
+
+FileStream fileStream = null;
+
+var request = client.FileDownload(fileInfo);
+
+CancellationTokenSource cancellationTokenSource = new();
+
+int count = 0;
+decimal chunkSize = 0;
+while (await request.ResponseStream.MoveNext(cancellationTokenSource.Token))
+{
+    if (count++ == 0)
+    {
+        fileStream = new FileStream(@$"{downloadPath}/{request.ResponseStream.Current.Info.FileName}{request.ResponseStream.Current.Info.FileExtension}", FileMode.CreateNew);
+        fileStream.SetLength(request.ResponseStream.Current.FileSize);
+    }
+
+    var buffer = request.ResponseStream.Current.Buffer.ToByteArray();
+    await fileStream.WriteAsync(buffer, 0, request.ResponseStream.Current.ReadedByte);
+
+    Console.WriteLine($"{Math.Round(((chunkSize += request.ResponseStream.Current.ReadedByte) * 100) / request.ResponseStream.Current.FileSize)}%");
+}
+
+Console.WriteLine("Yüklendi...");
+await fileStream.DisposeAsync();
+fileStream.Close();
+
+
+
---
+++ gRPC_Upload_Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using Google.Protobuf;
+using Grpc.Net.Client;
+using gRPC_Upload_Client.Protos;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5263");
+var client = new FileService.FileServiceClient(channel);
+
+string file = @"/Users/dilanozer/Downloads/beceri.mp4";
+
+using FileStream fileStream = new FileStream(file, FileMode.Open);
+var content = new BytesContent
+{
+    FileSize = fileStream.Length,
+    ReadedByte = 0,
+    Info = new gRPC_Upload_Client.Protos.FileInfo {
+        FileName = Path.GetFileNameWithoutExtension(fileStream.Name),
+        FileExtension = Path.GetExtension(fileStream.Name)}
+};
+
+var upload = client.FileUpload();
+byte[] buffer = new byte[2048];
+while ((content.ReadedByte = await fileStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+{
+    content.Buffer = ByteString.CopyFrom(buffer);
+    await upload.RequestStream.WriteAsync(content);
+}
+
+await upload.RequestStream.CompleteAsync();
+fileStream.Close();
+
